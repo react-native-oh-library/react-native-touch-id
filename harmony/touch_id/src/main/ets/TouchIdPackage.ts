ce289b95097a892e60306e601e19ad5c7488aed2
wushuwang <wushuwang@h-partners.com>
2024-06-19 19:32:36 +0800

    refactor: 代码格式化

--- harmony/touch_id/src/main/ets/TouchIdPackage.ts
+++ harmony/touch_id/src/main/ets/TouchIdPackage.ts
@@ -3,25 +3,21 @@ import { TM } from "@rnoh/react-native-openharmony/generated/ts"
 import type {TurboModule ,TurboModuleContext} from '@rnoh/react-native-openharmony/ts'
 import { TouchIdTuboModule } from './TouchIdTuboModule'
 
-
 class  TouchIdModuleFactory extends  TurboModulesFactory {
   createTurboModule(name: string): TurboModule | null {
     if (this.hasTurboModule(name)) {
       return new TouchIdTuboModule(this.ctx);
     }
-
     return null
   }
 
   hasTurboModule(name: string): boolean {
-
     return name === TM.NativeHarmonyTouchId.NAME
   }
 }
 
 export  class  TouchIdPackage extends  RNPackage {
   createTurboModulesFactory(ctx: RNOHContext): TurboModulesFactory {
-    return new TouchIdModuleFactory(ctx
-    )
+    return new TouchIdModuleFactory(ctx)
   }
 }
