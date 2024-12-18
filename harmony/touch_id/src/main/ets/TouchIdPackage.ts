e1b943feecb525f365096f5aec0afabaa2282689
BigDream <18795973943@163.com>
2024-12-18 10:24:15 +0800

    chore: 五项核查整改 (#11)
    Co-authored-by: wushuwang <wushuwang1@h-partners.com>

--- harmony/touch_id/src/main/ets/TouchIdPackage.ts
+++ harmony/touch_id/src/main/ets/TouchIdPackage.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { TurboModulesFactory ,RNPackage, UITurboModuleContext } from '@rnoh/react-native-openharmony/ts';
 import { TM } from "@rnoh/react-native-openharmony/generated/ts"
 import type {TurboModule ,TurboModuleContext} from '@rnoh/react-native-openharmony/ts'
--- harmony/touch_id/src/main/ets/TouchIdTuboModule.ts
+++ harmony/touch_id/src/main/ets/TouchIdTuboModule.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { TurboModule } from '@rnoh/react-native-openharmony/ts';
 import { TM } from "@rnoh/react-native-openharmony/generated/ts"
 import userIAM_userAuth from '@ohos.userIAM.userAuth';
